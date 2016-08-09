223778928e16ca4e124d212d0b7c58d1146e9246
NicayAbubakar <nicaygwapa@gmail.com>
2016-08-09 19:04:12 +0800

    WelcomeForm

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name:""
+  };
+  utrohon = (event)=>{
+    var inputName = event.target.value;
+    this.setState({
+      name: inputName
+    });
+  };
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React-{this.state.name}</h2>
         </div>
         <p className="App-intro">
-         WISHART <code>WISHART</code> WISHART
+         <form>
+         Enter your name:
+         <input type = "text" onChange={this.utrohon}/>
+         
+         </form>
         </p>
       </div>
     );
